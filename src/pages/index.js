14acc0dc2ad201eb3ba81575bf2066f9d3189c31
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-01-04 15:41:03 -0500

    Add initial styled 'Hello world!' div

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import styled from 'styled-components'
 
-export default () => <div>Hello world!</div>
+const StyledDiv = styled.div`
+    text-align: center;
+    margin: 0 auto;
+    height: 97vh;
+    line-height: 97vh;
+    font-family: helvetica;
+    font-weight: bold;
+    color: #725A93 ;
+    font-size: 50px;
+    background: #ECE4F9;
+
+    transition: font-size 2s ease-in-out, color 2s ease-in-out;
+
+    &:hover{
+        font-size: 65px;
+        color: #66119A;
+    }
+`
+
+export default () => <StyledDiv>Hello world!</StyledDiv>
